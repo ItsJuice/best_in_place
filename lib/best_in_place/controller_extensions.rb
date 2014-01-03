702b49aac47a2e80341c6b3f4ad54e2c32b47b18
Liam Bennett <liam@littleowllabs.com>
2014-01-03 17:03:22 +0000

    Adding controller options to allow STI

--- lib/best_in_place/controller_extensions.rb
+++ lib/best_in_place/controller_extensions.rb
@@ -5,13 +5,15 @@ module BestInPlace
     end
 
   private
-    def respond_bip_ok(obj)
-      if obj.respond_to?(:id)
+    def respond_bip_ok(obj, options)
+      if options[:class_name]
+        klass = options[:class_name]
+      elsif obj.respond_to?(:id)
         klass = "#{obj.class}_#{obj.id}"
       else
         klass = obj.class.to_s
       end
-      param_key = BestInPlace::Utils.object_to_key(obj)
+      param_key = options[:param_key] || BestInPlace::Utils.object_to_key(obj)
       updating_attr = params[param_key].keys.first
 
       if renderer = BestInPlace::DisplayMethods.lookup(klass, updating_attr)
